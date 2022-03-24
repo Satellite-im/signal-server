ece696b47d8f77f431407599bcc3822a30d9ebe7
Chris Hogan <chris@hogan.io>
2022-03-24 16:44:27 -0600

    first commit - generic signal server

--- index.js
+++ index.js
@@ -8,34 +8,16 @@ var server = new Server({
   http: true, // enable http server? [default=true]
   ws: true, // enable websocket server? [default=true]
   stats: true, // enable web-based statistics? [default=true]
-  filter: function (infoHash, params, cb) {
-    // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
-    // omitted, all torrents are allowed. It is possible to interface with a database or
-    // external system before deciding to allow/deny, because this function is async.
+  // filter: function (infoHash, params, cb) {
+  //   // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
+  //   // omitted, all torrents are allowed. It is possible to interface with a database or
+  //   // external system before deciding to allow/deny, because this function is async.
 
-    // It is possible to block by peer id (whitelisting torrent clients) or by secret
-    // key (private trackers). Full access to the original HTTP/UDP request parameters
-    // are available in `params`.
-
-    // This example only allows one torrent.
-    console.log(infoHash)
-    cb(null)
-    // var allowed = (infoHash === 'aaa67059ed6bd08362da625b3ae77f6f4a075aaa')
-    // if (allowed) {
-    //   // If the callback is passed `null`, the torrent will be allowed.
-    //   cb(null)
-    // } else {
-    //   // If the callback is passed an `Error` object, the torrent will be disallowed
-    //   // and the error's `message` property will be given as the reason.
-    //   cb(new Error('disallowed torrent'))
-    // }
-  }
+  //   // It is possible to block by peer id (whitelisting torrent clients) or by secret
+  //   // key (private trackers). Full access to the original HTTP/UDP request parameters
+  //   // are available in `params`.
+  // }
 })
-
-// Internal http, udp, and websocket servers exposed as public properties.
-// server.http
-// server.udp
-// server.ws
 
 server.on('error', function (err) {
   // fatal server error!
@@ -54,27 +36,4 @@ server.on('listening', function () {
 })
 
 // start tracker server listening! Use 0 to listen on a random free port.
-console.log('port ', port)
 server.listen(port)
-
-// listen for individual tracker messages from peers:
-
-server.on('start', function (addr) {
-  console.log('got start message from ' + addr)
-})
-
-server.on('complete', function (addr) {})
-server.on('update', function (addr) {})
-server.on('stop', function (addr) {})
-
-// // get info hashes for all torrents in the tracker server
-// Object.keys(server.torrents)
-
-// // get the number of seeders for a particular torrent
-// server.torrents[infoHash].complete
-
-// // get the number of leechers for a particular torrent
-// server.torrents[infoHash].incomplete
-
-// // get the peers who are in a particular torrent swarm
-// server.torrents[infoHash].peers
